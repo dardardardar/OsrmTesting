80d37642e140c68a815bb5d77b367cdbde89055b
satrioinfotech <satrio@infotech.co.id>
2024-07-04 09:45:48 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter_map_mbtiles/flutter_map_mbtiles.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:mbtiles/mbtiles.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter_map_geojson/flutter_map_geojson.dart';
 
 void main() {
   runApp(const MyApp());
@@ -57,29 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return MbTiles(mbtilesPath: file.path);
   }
 
-  Future<String> geoJson() async {
-    String data =
-        await DefaultAssetBundle.of(context).loadString("assets/Trees.json");
-    return data;
-  }
-
   @override
   Widget build(BuildContext context) {
-    GeoJsonParser myGeoJson = GeoJsonParser();
-
-    myGeoJson.parseGeoJsonAsString('''{
-"type": "FeatureCollection",
-"name": "Treee",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 1, "Lat": null, "Long": null }, "geometry": { "type": "Point", "coordinates": [ 112.763499768510727, -1.484361723978728 ] } },
-{ "type": "Feature", "properties": { "id": 2, "Lat": null, "Long": null }, "geometry": { "type": "Point", "coordinates": [ 112.763517963533644, -1.483911548230743 ] } },
-{ "type": "Feature", "properties": { "id": 3, "Lat": null, "Long": null }, "geometry": { "type": "Point", "coordinates": [ 112.763467927220617, -1.483634167168685 ] } },
-{ "type": "Feature", "properties": { "id": 4, "Lat": null, "Long": null }, "geometry": { "type": "Point", "coordinates": [ 112.763536158556548, -1.485112016688344 ] } },
-{ "type": "Feature", "properties": { "id": 1, "Lat": "s", "Long": "s" }, "geometry": { "type": "Point", "coordinates": [ 112.763488161015005, -1.48461609517838 ] } }
-]
-}''');
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -92,14 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
             _mbtiles = snapshot.data;
             final metadata = _mbtiles!.getMetadata();
             return Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Text(
-                    'MBTiles Name: ${metadata.name}, '
-                    'Format: ${metadata.format}',
-                  ),
-                ),
+              children: [
                 Expanded(
                   child: FlutterMap(
                     options: const MapOptions(
@@ -116,7 +87,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           silenceTileNotFound: true,
                         ),
                       ),
-                      MarkerLayer(markers: myGeoJson.markers)
                     ],
                   ),
                 ),
@@ -134,7 +104,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void dispose() {
-    // close the open database connection
     _mbtiles?.dispose();
     super.dispose();
   }
